58f6e8dc5cfe54ac80f8f9a39df18586404bfa75
Shari <shari9@uab.edu>
2020-11-05 21:48:38 -0600

    saveone

--- ChallengeSets/ChallengeSet01.cs
+++ ChallengeSets/ChallengeSet01.cs
@@ -17,27 +17,38 @@ namespace ChallengeSets
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return minuend - subtrahend;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            public static int Sum(int num1, int num2)
+            {
+                int total;
+                total = num1 + num2;
+                return total;
+            }
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            int GetSmallestNumber = inputs[0];
+            foreach(var input in inputs)
+                if(input < smallest) smallest = input;
+                return smallest;
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            public long Multiply(long factor1, long factor2)
+            Console.WriteLine("Multiplication Operator: "+ result);
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            string custName = cust.FirstName + " " + cust.LastName;
+            custName = custName.Trim();
+            salutation + " " + custName + ":";
         }
 
         public string GetHey()
